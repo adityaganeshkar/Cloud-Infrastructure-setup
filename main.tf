cc4f8a94f9038b8dab9e83af93c49b1e1694a553
adityaganeshkar <ganeshkaraditya143@gmail.com>
2024-02-09 17:18:09 +0530

    ingress rules updated

--- main.tf
+++ main.tf
@@ -85,7 +85,22 @@ resource "aws_security_group" "instance_sg" {
     cidr_blocks = ["0.0.0.0/0"]  # Allow SSH from anywhere (for demonstration purposes)
   }
 
-  # Add additional ingress rules as needed
+  # Define ingress rules for nginx
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  
 }
 
 # Define the EC2 instance in Public Instance
