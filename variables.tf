46d9b2f934be41b811e7222ab33aa3993b81f19b
adityaganeshkar <ganeshkaraditya143@gmail.com>
2024-02-14 18:43:56 +0530

    variables added

--- variables.tf
+++ variables.tf
@@ -2,3 +2,14 @@
 locals {
   instance_ami= {"Instance-1":"ami-05fb0b8c1424f266b","Instance-2":"ami-05fb0b8c1424f266b"}
 }
+
+# Instance Type modification
+variable "instance_type" {
+  default = "t2.micro"
+  
+}
+
+# Instance key
+variable "instance_key"{
+  default = "Q-iam-key-us-east2"
+} 
